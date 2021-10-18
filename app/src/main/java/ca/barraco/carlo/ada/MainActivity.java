660ad2c19962a159daba0909fe5e8fb1911bbb84
Carlo Barraco <carlobarraco@gmail.com>
2021-10-17 20:40:08 -0400

    Added chat log style interface for requests and replies

--- app/src/main/java/ca/barraco/carlo/ada/MainActivity.java
+++ app/src/main/java/ca/barraco/carlo/ada/MainActivity.java
@@ -1,40 +1,74 @@
 package ca.barraco.carlo.ada;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.speech.RecognitionListener;
 import android.speech.SpeechRecognizer;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.ArrayList;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import ca.barraco.carlo.ada.databinding.ActivityMainBinding;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String ACTION_START_RECOGNIZING = "MainActivity.ACTION_START_RECOGNIZING";
+    private static final String ACTION_START_LISTENING = "MainActivity.ACTION_START_LISTENING";
+    private static final String ACTION_STOP_LISTENING = "MainActivity.ACTION_STOP_LISTENING";
+    private static final String ACTION_SHOW_RECOGNITION_RESULT = "MainActivity.ACTION_SHOW_RECOGNITION_RESULT";
+    private static final String ACTION_SHOW_REPLY = "MainActivity.ACTION_SHOW_REPLY";
+    private static final String EXTRA_MESSAGE = "MainActivity.EXTRA_MESSAGE";
+    private static final String[] Actions = {
+            ACTION_START_RECOGNIZING,
+            ACTION_START_LISTENING,
+            ACTION_STOP_LISTENING,
+            ACTION_SHOW_RECOGNITION_RESULT,
+            ACTION_SHOW_REPLY
+    };
     private TextView statusView;
     private boolean fromAssistantButton;
+    private LinearLayout chatLayout;
+
+    public static void startRecognizing(Context context) {
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
+        Intent intent = new Intent(ACTION_START_RECOGNIZING);
+        localBroadcastManager.sendBroadcast(intent);
+    }
+
+    public static void startListening(Context context) {
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
+        Intent intent = new Intent(ACTION_START_LISTENING);
+        localBroadcastManager.sendBroadcast(intent);
+    }
+
+    public static void stopListening(Context context) {
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
+        Intent intent = new Intent(ACTION_STOP_LISTENING);
+        localBroadcastManager.sendBroadcast(intent);
+    }
+
+    public static void showRecognitionResult(Context context, String reply) {
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
+        Intent intent = new Intent(ACTION_SHOW_RECOGNITION_RESULT);
+        intent.putExtra(EXTRA_MESSAGE, reply);
+        localBroadcastManager.sendBroadcast(intent);
+    }
+
+    public static void showReply(Context context, String reply) {
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
+        Intent intent = new Intent(ACTION_SHOW_REPLY);
+        intent.putExtra(EXTRA_MESSAGE, reply);
+        localBroadcastManager.sendBroadcast(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +83,22 @@ public class MainActivity extends AppCompatActivity {
 
         statusView = findViewById(R.id.statusView);
 
+        chatLayout = findViewById(R.id.chatLayout);
+
         binding.fab.setOnClickListener(view -> startListening());
+
+        setUpBroadcastReceiver();
+    }
+
+    private void setUpBroadcastReceiver() {
+        IntentFilter intentFilter = new IntentFilter();
+        for (String action : Actions) {
+            intentFilter.addAction(action);
+        }
+
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        MainActivityBroadcastReceiver mainActivityBroadcastReceiver = new MainActivityBroadcastReceiver();
+        localBroadcastManager.registerReceiver(mainActivityBroadcastReceiver, intentFilter);
     }
 
     private void handleAssistantButton() {
@@ -81,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         SpeechRecognizer speechRecognizer = SpeechRecognizer.createSpeechRecognizer(context);
         Intent recognizerIntent = new Intent();
-        speechRecognizer.setRecognitionListener(new MyRecognitionListener());
+        speechRecognizer.setRecognitionListener(new MyRecognitionListener(this));
         speechRecognizer.startListening(recognizerIntent);
     }
 
@@ -105,137 +154,103 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    private class MyRecognitionListener implements RecognitionListener {
+    public class MainActivityBroadcastReceiver extends BroadcastReceiver {
 
         @Override
-        public void onReadyForSpeech(Bundle bundle) {
+        public void onReceive(Context context, @NonNull Intent intent) {
+            String action = intent.getAction();
+            if (action == null) {
+                Logger.warning("Received null action");
+                return;
+            }
+
+            Logger.debug("Handling %s", action);
+            if (action.equals(ACTION_START_LISTENING)) {
+                handleStartListening();
+            } else if (action.equals(ACTION_STOP_LISTENING)) {
+                handleStopListening();
+            } else if (action.equals(ACTION_START_RECOGNIZING)) {
+                handleStartRecognizing();
+            } else if (action.equals(ACTION_SHOW_RECOGNITION_RESULT)) {
+                handleShowRecognitionResult(intent);
+            } else if (action.equals(ACTION_SHOW_REPLY)) {
+                handleShowReply(intent);
+            }
+        }
+
+        private void handleStartListening() {
             statusView.setText(R.string.recognition_listening);
         }
 
-        @Override
-        public void onBeginningOfSpeech() {
+        private void handleStartRecognizing() {
             statusView.setText(R.string.recognition_recognizing);
         }
 
-        @Override
-        public void onRmsChanged(float v) {
-            // not used
-        }
-
-        @Override
-        public void onBufferReceived(byte[] bytes) {
-            // not used
-        }
-
-        @Override
-        public void onEndOfSpeech() {
+        private void handleStopListening() {
             statusView.setText(R.string.recognition_idle);
         }
 
-        @Override
-        public void onError(int i) {
-            // not used
+        private void handleShowRecognitionResult(@NonNull Intent intent) {
+            String message = intent.getStringExtra(EXTRA_MESSAGE);
+            if (message == null) {
+                Logger.warning("Received null message");
+                return;
+            }
+
+            Logger.debug("Received message: %s", message);
+            if (fromAssistantButton) {
+                finish();
+            } else {
+                TextView textView = getTextView(message);
+
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                params.gravity = Gravity.END;
+                params.topMargin = spToPixels(chatLayout.getContext(), 6);
+                textView.setLayoutParams(params);
+                textView.setBackgroundColor(getResources().getColor(R.color.AdaRequest));
+                textView.setTextColor(getResources().getColor(R.color.Black));
+
+                chatLayout.addView(textView);
+            }
         }
 
-        @Override
-        public void onResults(Bundle bundle) {
-            ArrayList<String> recognitionResults = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            String speechRecognitionResult = recognitionResults.get(0);
-            sendConversationText(speechRecognitionResult);
-            Logger.information("Recognized speech: " + speechRecognitionResult);
-        }
+        private void handleShowReply(@NonNull Intent intent) {
+            String message = intent.getStringExtra(EXTRA_MESSAGE);
+            if (message == null) {
+                Logger.warning("Received null message");
+                return;
+            }
 
-        private void sendConversationText(String voiceRecognitionResult) {
-            try {
-                Request request = buildRequest(voiceRecognitionResult);
-                OkHttpClient okHttpClient = new OkHttpClient();
-                Call call = okHttpClient.newCall(request);
-                Callback callback = new MyCallback();
-                call.enqueue(callback);
-            } catch (Exception e) {
-                Logger.error("Error sending conversation", e);
+            Logger.debug("Received message: %s", message);
+            if (fromAssistantButton) {
+                finish();
+            } else {
+                TextView textView = getTextView(message);
+
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                params.gravity = Gravity.START;
+                params.topMargin = spToPixels(chatLayout.getContext(), 6);
+                textView.setLayoutParams(params);
+                textView.setBackgroundColor(getResources().getColor(R.color.HomeAssistant));
+                textView.setTextColor(getResources().getColor(R.color.White));
+
+                chatLayout.addView(textView);
             }
         }
 
         @NonNull
-        private Request buildRequest(String voiceRecognitionResult) {
-            // TODO validate url
-            String homeAssistantUrl = getServerAddressFromPreferences() + "/api/conversation/process";
-
-            String authorizationHeaderName = "Authorization";
-            String homeAssistantToken = getHomeAssistantTokenFromPreferences();
-            String authorizationHeaderValue = "Bearer " + homeAssistantToken;
-
-            MediaType requestMediaType = MediaType.get("application/json; charset=utf-8");
-            String requestJsonBody = "{\"text\":\"" + voiceRecognitionResult + "\", \"conversation_id\":\"ada\"}";
-            RequestBody requestBody = RequestBody.create(requestJsonBody, requestMediaType);
-
-            return new Request.Builder()
-                    .url(homeAssistantUrl)
-                    .header(authorizationHeaderName, authorizationHeaderValue)
-                    .post(requestBody)
-                    .build();
+        private TextView getTextView(String message) {
+            TextView textView = new TextView(chatLayout.getContext());
+            int padding = spToPixels(chatLayout.getContext(), 6);
+            textView.setPadding(padding, padding, padding, padding);
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+            textView.setText(message);
+            return textView;
         }
 
-        private String getServerAddressFromPreferences() {
-            Context context = getApplicationContext();
-            String webServerAddressKey = context.getString(R.string.home_assistant_url_key);
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-            return sharedPreferences.getString(webServerAddressKey, "");
-        }
-
-        private String getHomeAssistantTokenFromPreferences() {
-            Context context = getApplicationContext();
-            String homeAssistantTokenKey = context.getString(R.string.home_assistant_access_token_key);
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-            return sharedPreferences.getString(homeAssistantTokenKey, "");
-        }
-
-        @Override
-        public void onPartialResults(Bundle bundle) {
-            // not used
-        }
-
-        @Override
-        public void onEvent(int i, Bundle bundle) {
-            // not used
-        }
-    }
-
-    private class MyCallback implements Callback {
-        @Override
-        public void onFailure(@NonNull Call call, @NonNull IOException e) {
-            // TODO implement failure handling for HTTP call
-            // not used yet
-        }
-
-        @Override
-        public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-            // TODO show response to user
-            String reply = parseResponse(response);
-            Logger.debug("Got reply from Home Assistant: " + reply);
-
-            // exit immediately if triggered from assistant button
-            if (fromAssistantButton) {
-                finish();
-            }
-        }
-
-        @Nullable
-        private String parseResponse(@NonNull Response response) throws IOException {
-            try {
-                ResponseBody responseBody = response.body();
-                if (responseBody != null) {
-                    String responseJsonBody = responseBody.string();
-                    JSONObject responseJsonObject = new JSONObject(responseJsonBody);
-                    return responseJsonObject.getJSONObject("speech").getJSONObject("plain").getString("speech");
-                }
-                return null;
-            } catch (JSONException exception) {
-                Logger.error("Failed to parse response from Home Assistant", exception);
-                return null;
-            }
+        private int spToPixels(@NonNull Context context, int sp) {
+            float scaledDensity = context.getResources().getDisplayMetrics().scaledDensity;
+            return (int) (sp * scaledDensity);
         }
     }
 }
---
+++ app/src/main/java/ca/barraco/carlo/ada/MyRecognitionListener.java
@@ -0,0 +1,159 @@
+package ca.barraco.carlo.ada;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.SpeechRecognizer;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+public class MyRecognitionListener implements RecognitionListener {
+
+    private final Context context;
+
+    public MyRecognitionListener(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle bundle) {
+        MainActivity.startListening(context);
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+        MainActivity.startRecognizing(context);
+    }
+
+    @Override
+    public void onRmsChanged(float v) {
+        // not used
+    }
+
+    @Override
+    public void onBufferReceived(byte[] bytes) {
+        // not used
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        MainActivity.stopListening(context);
+    }
+
+    @Override
+    public void onError(int i) {
+        // not used
+    }
+
+    @Override
+    public void onResults(Bundle bundle) {
+        ArrayList<String> recognitionResults = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String speechRecognitionResult = recognitionResults.get(0);
+        sendConversationText(speechRecognitionResult);
+        MainActivity.showRecognitionResult(context, speechRecognitionResult);
+        Logger.information("Recognized speech: " + speechRecognitionResult);
+    }
+
+    private void sendConversationText(String voiceRecognitionResult) {
+        try {
+            Request request = buildRequest(voiceRecognitionResult);
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Call call = okHttpClient.newCall(request);
+            Callback callback = new MyCallback();
+            call.enqueue(callback);
+        } catch (Exception e) {
+            Logger.error("Error sending conversation", e);
+        }
+    }
+
+    @NonNull
+    private Request buildRequest(String voiceRecognitionResult) {
+        // TODO validate url
+        String homeAssistantUrl = getServerAddressFromPreferences() + "/api/conversation/process";
+
+        String authorizationHeaderName = "Authorization";
+        String homeAssistantToken = getHomeAssistantTokenFromPreferences();
+        String authorizationHeaderValue = "Bearer " + homeAssistantToken;
+
+        MediaType requestMediaType = MediaType.get("application/json; charset=utf-8");
+        String requestJsonBody = "{\"text\":\"" + voiceRecognitionResult + "\", \"conversation_id\":\"ada\"}";
+        RequestBody requestBody = RequestBody.create(requestJsonBody, requestMediaType);
+
+        return new Request.Builder()
+                .url(homeAssistantUrl)
+                .header(authorizationHeaderName, authorizationHeaderValue)
+                .post(requestBody)
+                .build();
+    }
+
+    private String getServerAddressFromPreferences() {
+        String webServerAddressKey = context.getString(R.string.home_assistant_url_key);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getString(webServerAddressKey, "");
+    }
+
+    private String getHomeAssistantTokenFromPreferences() {
+        String homeAssistantTokenKey = context.getString(R.string.home_assistant_access_token_key);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getString(homeAssistantTokenKey, "");
+    }
+
+    @Override
+    public void onPartialResults(Bundle bundle) {
+        Logger.debug("Got partial results");
+    }
+
+    @Override
+    public void onEvent(int i, Bundle bundle) {
+        // not used
+    }
+
+    private class MyCallback implements Callback {
+        @Override
+        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+            // TODO implement failure handling for HTTP call
+            // not used yet
+        }
+
+        @Override
+        public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+            String reply = parseResponse(response);
+            Logger.debug("Got reply from Home Assistant: " + reply);
+            MainActivity.showReply(context, reply);
+        }
+
+        @Nullable
+        private String parseResponse(@NonNull Response response) throws IOException {
+            try {
+                ResponseBody responseBody = response.body();
+                if (responseBody != null) {
+                    String responseJsonBody = responseBody.string();
+                    JSONObject responseJsonObject = new JSONObject(responseJsonBody);
+                    return responseJsonObject.getJSONObject("speech").getJSONObject("plain").getString("speech");
+                }
+                return null;
+            } catch (JSONException exception) {
+                Logger.error("Failed to parse response from Home Assistant", exception);
+                return null;
+            }
+        }
+    }
+}
